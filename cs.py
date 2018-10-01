cadb11b1d33c56ff24ef87666c66e76744aa40fa
Nightwolf <superglevins@mail.ru>
2018-10-02 01:26:43 +0300

    changed arch a bit, added some OOP

--- cs.py
+++ cs.py
@@ -3,8 +3,110 @@ import subprocess
 import os, fnmatch
 import sys, sh
 import argparse
-import terminaltables
+import shutil
+import json
+from terminaltables import DoubleTable
 
+
+config_path = os.path.expanduser("~/Documents/CSCOPE/config.json")
+
+
+def ccwrap(code):
+    def inner(text, bold=False):
+        cc = code
+        if bold:
+            cc = '1;{0}'.format(cc)
+        strs = text.split('\n')
+        nl = len(strs)
+        if nl > 1:
+            ret = ''
+            for str in strs:
+                ret += '\033[%sm%s\033[0m\n' % (cc, str)
+            ret = ret[:-1]
+            return ret
+        else:
+            return "\033[%sm%s\033[0m" % (cc, text)
+    return inner
+
+
+green = ccwrap('32')
+white = ccwrap('37')
+
+
+class Project:
+    def __init__(self, id, name, path, is_active, root_path):
+        self.id = id
+        self.name = name
+        self.path = path
+        self.is_active = is_active
+        self.root_path = root_path + name
+
+    def delete(self):
+        shutil.rmtree(self.root_path)
+
+    @property
+    def to_json(self):
+        return {
+                'id': self.id,
+                'path': self.path,
+                'is_active': self.is_active
+               }
+    @property
+    def to_row(self):
+        color = green if self.is_active else white
+        return [
+                color(str(self.id)),
+                color(self.name),
+                color(self.path)
+               ]
+
+
+class Profile:
+    def __init__(self):
+        self.data = []
+        self.path = ""
+        
+    def load(self, path):
+        with open(path, 'r') as fp:
+            config = json.load(fp)
+
+        self.path = config["path"]
+        config = config["Projects"]
+
+        for p in config:
+            pr = Project(name=p,
+                         id=config[p]["id"],
+                         path=config[p]["path"],
+                         is_active=config[p]["is_active"],
+                         root_path=self.path)
+            self.data.append(pr)
+        
+    def save(self, path):
+        json_dic = {
+                    "Projects": {p.name: p.to_json for p in self.data},
+                    "path": self.path
+                   }
+        with open(path, 'w') as fp:
+            json.dump(json_dic, fp, sort_keys=True, indent=4)
+
+    def delete_project(self, name):
+        for p in self.data:
+            if p.name == name:
+                p.delete()
+                self.data.remove(p)
+                return True
+
+        return False
+
+    @property
+    def table(self):
+        table_list = [[white("ID"), white("Name"), white("Path")]]
+        for p in self.data:
+            table_list.append(p.to_row)
+        
+        table = DoubleTable(table_list, title=white('Projects', True))
+        table.inner_row_border = True
+        return table
 
 def get_files_from_dir(dir):
     list = []
@@ -44,18 +146,27 @@ def cs_create(args):
 
     os.chdir(path_name)
     subprocess.call(["cscope", "-b", "-i", path_name + "/cscope.files"])
-    exec(open("/home/nightwolf/Documents/CSCOPE/set_env.sh").read())
-#    subprocess.call(["source", "~/Documents/CSCOPE/set_env.sh"], shell=True)
-
 
 def cs_delete(args):
-    print(2)
+    profile = Profile()
+    profile.load(config_path)
+
+    ret = profile.delete_project(args.name)
+    if not ret:
+        print("'{0}' project doesn't exist".format(args.name))
+    else:
+        print("Success")
+        profile.save(config_path)
+
 
 def cs_refresh(args):
     print(3)
 
 def cs_show(args):
-    print(4)
+    profile = Profile()
+    profile.load(config_path)
+    print(profile.table.table)
+
 
 def main(args=None):
     parser = argparse.ArgumentParser(
